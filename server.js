45f5264172fdabde63f26825c8efa23664ccf70d
Joseph Lago <josephlago90@gmail.com>
2016-03-02 16:10:10 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.listen(9000)
+
+var messages = ["Hello", "This is a test", "All your base are belong to us"];
+
+app.get('/', function(req, res, next) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+})
+
+app.post('/', function(req, res, next) {
+  messages.push(req.body.message);
+
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+})
